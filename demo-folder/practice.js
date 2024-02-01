8474368784f8ad11678e8f8ecd0d9b1adbf0ef54
ZacharyDagnall <zachary.dagnall@gmail.com>
2024-02-01 08:26:35 -0700

    add practice files

---
+++ demo-folder/practice.js
@@ -0,0 +1,9 @@
+function mysterious_function(num, thing) {
+  return `There are ${num} ${thing}s`;
+}
+
+console.log(mysterious_function(12, "apple"));
+console.log(mysterious_function(9, "banana"));
+console.log(mysterious_function(3, "mouse"));
+console.log(mysterious_function(1, "orange"));
+console.log(mysterious_function("11", "coyotes"));
